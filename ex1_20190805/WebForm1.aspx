367ebc6b2d8f8c638a8ab54fe68aa1992fb16c3f
ZHE HUA <ZHE HUA@LAPTOP-30VO9BI8>
2019-08-05 15:11:16 +0800

    加入專案檔案。

---
+++ ex1_20190805/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ex1_20190805.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+                <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+                <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+                <RowStyle BackColor="White" ForeColor="#330099" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+                <SortedAscendingCellStyle BackColor="#FEFCEB" />
+                <SortedAscendingHeaderStyle BackColor="#AF0101" />
+                <SortedDescendingCellStyle BackColor="#F6F0C0" />
+                <SortedDescendingHeaderStyle BackColor="#7E0000" />
+            </asp:GridView>
+            <br />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ ex1_20190805/WebForm1.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace ex1_20190805
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            #region dt
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Age", typeof(int));
+            dt.Columns.Add("Addr");
+            dt.Columns.Add("Mobile", typeof(string));
+            dt.Columns.Add("birthday", typeof(DateTime));
+            dt.TableName = "dt1";
+            DataRow workRow = dt.NewRow();
+            workRow["Name"] = "Ken";
+            workRow["Age"] = 10;
+            workRow["Addr"] = "Taipei";
+            workRow["Mobile"] = "0912345678";
+            workRow["Birthday"] = DateTime.Now;
+            dt.Rows.Add(workRow);
+            dt.AcceptChanges(); 
+            #endregion
+            #region ds
+            DataSet ds = new DataSet();//多個DataTabel
+            ds.Tables.Add(dt); 
+            #endregion
+            List<DataTable> Ldt = new List<DataTable>();
+            Ldt.Add(dt);
+            //合併dt.Merge
+            #region SQL ADO
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString);
+            SqlDataReader dr = null;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandTimeout = 300;
+            cmd.Connection = conn;
+            #region Query
+            cmd.CommandText = "SELECT * FROM [Suppliers] WHERE [City]=@City or [Country]=@Country ORDER BY [CompanyName]";
+
+
+            cmd.Parameters.Add("@City", SqlDbType.NVarChar, 15);
+
+            cmd.Parameters["@City"].Value = "London";
+            cmd.Parameters.Add("@Country", SqlDbType.NVarChar, 15);
+
+            cmd.Parameters["@Country"].Value = "UK";
+            #endregion
+            #region UPD
+            //cmd.CommandText = "UPDATE [Suppliers] SET Fax=@Fax  WHERE [City]=@City and  [Country]=@Country ";
+
+            //cmd.Parameters.Add("@Fax", SqlDbType.NVarChar, 24);
+
+            //cmd.Parameters["@Fax"].Value = "0312345678";
+            //cmd.Parameters.Add("@City", SqlDbType.NVarChar, 15);
+
+            //cmd.Parameters["@City"].Value = "London";
+            //cmd.Parameters.Add("@Country", SqlDbType.NVarChar, 15);
+
+            //cmd.Parameters["@Country"].Value = "UK"; 
+            #endregion
+            try
+            {
+                conn.Open();
+                 dr = cmd.ExecuteReader(); //query
+
+                //cmd.ExecuteNonQuery();//way1 不太好
+                // int rc = cmd.ExecuteNonQuery();//way2
+                //資料繫結或做其他處理
+                GridView1.DataSource = dr;
+                
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally//關閉資源
+            {
+                if (dr != null)
+                {
+                    cmd.Cancel();
+                    dr.Close();
+                }
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                    conn.Dispose();
+
+                }
+
+            } 
+            #endregion
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+            
+
+
+        }
+
+        
+    }
+}
